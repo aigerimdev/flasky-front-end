2c968b3a60ef3f98f125eb7fd6b9eb4bfe598d6c
Aigerim Kalygulova <kalygulovas@gmail.com>
2025-06-06 10:49:30 -0700

    adds cat list and butoon

---
+++ src/components/Cat.jsx
@@ -0,0 +1,12 @@
+const Cat = () => {
+    return (
+        <>
+        <li>
+            <h2>Name of Cat</h2>
+            <p>Meow!</p>
+            <button>Pet</button>
+        </li>
+        </>
+    );
+};
+export default Cat;
---
+++ src/components/CatList.jsx
@@ -0,0 +1,15 @@
+import Cat from './Cat.jsx';
+const CatList = () => {
+    return (
+        <>
+            <h1>Cat List</h1>
+            <ul>
+                <Cat></Cat>
+                <Cat></Cat>
+                <Cat></Cat>
+                <Cat></Cat>
+            </ul>
+        </>
+    );
+};
+export default CatList;
